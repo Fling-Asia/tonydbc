58c0d64ef1dc692232634d63b1f115a550eb0c26
MichaelCurrie <mcurrie@gmail.com>
2024-01-07 16:53:41 +0700

    bump version 1.0.0 -> 1.0.1

--- src/tonydbc/__init__.py
+++ src/tonydbc/__init__.py
@@ -13,7 +13,7 @@ import dotenv
 import pathlib
 from collections import Counter
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 # Needs an extra .. for some reason (I guess if this library is imported...)
 DEFAULT_ENV_FILEPATH = os.path.join(os.path.abspath(__file__), "..", "..", "..", ".env")
