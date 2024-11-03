4603549cb99250bd2a1d60623a592f72d0e28eb8
Michael Currie <mcurrie@fling.asia>
2024-11-03 22:20:25 +0700

    bump version 1.2.16 -> 1.2.17

--- src/tonydbc/__init__.py
+++ src/tonydbc/__init__.py
@@ -27,7 +27,7 @@ e.g.
 
 """
 
-__version__ = "1.2.16"
+__version__ = "1.2.17"
 
 # Include vanialla dotenv.load_dotenv to be comprehensive, but it's not really needed
 # since the version people should use is load_dotenvs
