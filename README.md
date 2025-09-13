abe4942851c87a4fba0091d0591da4d8ce2b1e00
Michael Currie <mcurrie@fling.asia>
2025-09-13 13:24:44 +0700

    bump version 1.3.2 -> 1.3.3

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # TonyDBC
 
-Latest version: 1.3.2
+Latest version: 1.3.3
 
 Available on PyPI: https://pypi.org/project/tonydbc/
 
--- src/tonydbc/__init__.py
+++ src/tonydbc/__init__.py
@@ -27,7 +27,7 @@ e.g.
 
 """
 
-__version__ = "1.3.2"
+__version__ = "1.3.3"
 
 # Public API - explicitly declare what should be available when importing this package
 __all__ = [
